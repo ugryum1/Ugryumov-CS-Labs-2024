98b02da8c64afd0a4d68f0954b34a52b10472bef
ugryum <n.a.ugryumov@gmail.com>
2024-10-02 13:29:51 +0300

    add main.cpp

--- main.cpp
+++ main.cpp
@@ -3,56 +3,34 @@
 #include <iostream>
 
 namespace {
-const char kFirstTaskSymbol = '1';
-const char kSecondTaskSymbol = '2';
-const char kThirdTaskSymbol = '3';
-const char kFourthTaskSymbol = '4';
-const char kExitProgramSymbol = 'e';
-const char kContinueProgramSymbol = 'y';
+const char kFirstTask = '1';
+const char kSecondTask = '2';
+const char kThirdTask = '3';
+const char kFourthTask = '4';
+const char kExitProgram = 'e';
+const char kContinueProgram = 'y';
 
-const int kBeginingCycleInFirstTask = 0;
-const int kCycleStepInFirstTask = 5;
-const int kNotDivisionRemainder = 0;
+const int kFive = 5;
+const int kTwo = 2;
 
-const int kPositiveNumbersBegin = 0;
+const int kLoopNotNegativeABegin = 2;
+const int kLoopNotNegativeAEnd = 8;
+const int kLoopStepNotNegativeA = 2;
+const int kLoopNegativeABegin = 3;
+const int kLoopNegativeAEnd = 9;
+const int kLoopStepNegativeA = 3;
 
-const int kBeginingCycleIfPositiveA = 2;
-const int kEndingCycleIfPositiveA = 8;
-const int kStepCycleIfPositiveA = 2;
-const int kBeginingCycleIfNegativeA = 3;
-const int kEndingCycleIfNegativeA = 9;
-const int kStepCycleIfNegativeA = 3;
+const int kColumnWidth = 12;
+const int kPrecision = 6;
 
-const double kBeginingCycleInThirdTask = 0;
-const double kEndingCycleInThirdTask = 1;
-const double kCycleStepInThirdTask = 0.2;
+const double kXStep = 0.2;
 
-const int kColumnWidth = 10;   // ширина столбцов
-const int kNumbersLength = 4;  // длина чисел
-
-const double kErrorRate = 0.000001;  // погрешность
+const double kEpsilon = 1e-6;
 
 const int kThreeSummands = 3;
 const int kFiveSummands = 5;
 const int kTenSummands = 10;
 }  // namespace
-
-void TaskSelection();
-
-bool SolveAgain() {
-    std::cout << "\nЕсли хотите ещё раз решить это задание, напишите 'y', "
-                 "иначе напишите 'n', вы перейдёте к выбору заданий."
-              << std::endl;
-    char choice{};
-    std::cin >> choice;
-
-    if (choice == kContinueProgramSymbol) {
-        return true;
-    }
-
-    TaskSelection();
-    return false;
-}
 
 void DoFirstTask() {
     int n{};
@@ -60,10 +38,15 @@ void DoFirstTask() {
     std::cout << "\nВведите натуральные числа n и m (m < n) через проблел." << std::endl;
     std::cin >> n >> m;
 
+    if (m >= n || n <= 0 || m <= 0) {
+        std::cout << "Неверный ввод. Число m должно быть маньше числа n. Также оба числа дожны быть натуральными." << std::endl;
+        return;
+    }
+
     int answer = 0;
 
-    for (int i = kBeginingCycleInFirstTask; i <= n; i += kCycleStepInFirstTask) {
-        if (i % m != kNotDivisionRemainder) {
+    for (int i = 0; i <= n; i += kFive) {
+        if (i % m != 0) {
             answer += i;
         }
     }
@@ -72,10 +55,6 @@ void DoFirstTask() {
               << " одновременно, "
                  "на отрезке от 1 до "
               << n << " равна " << answer << "." << std::endl;
-
-    if (SolveAgain()) {
-        DoFirstTask();
-    }
 }
 
 void DoSecondTask() {
@@ -85,51 +64,42 @@ void DoSecondTask() {
 
     double answer = 1;
 
-    if (a >= kPositiveNumbersBegin) {
-        for (int i = kBeginingCycleIfPositiveA; i <= kEndingCycleIfPositiveA; i += kStepCycleIfPositiveA) {
-            answer *= i * i - a;
+    if (a >= 0) {
+        for (int i = kLoopNotNegativeABegin; i <= kLoopNotNegativeAEnd; i += kLoopStepNotNegativeA) {
+            answer *= std::pow(i, 2);
         }
+        answer -= a;
     } else {
-        for (int i = kBeginingCycleIfNegativeA; i <= kEndingCycleIfNegativeA; i += kStepCycleIfNegativeA) {
-            answer *= i * i - kStepCycleIfNegativeA;
+        for (int i = kLoopNegativeABegin; i <= kLoopNegativeAEnd; i += kLoopStepNegativeA) {
+            answer *= (i - kTwo);
         }
     }
 
     std::cout << "S = " << answer << std::endl;
-
-    if (SolveAgain()) {
-        DoSecondTask();
-    }
 }
 
 void DoThirdTask() {
     std::cout << std::setw(kColumnWidth) << "x" << std::setw(kColumnWidth) << "Y(x)" << std::setw(kColumnWidth) << "S(x)" << std::setw(kColumnWidth)
               << "N" << std::endl;
 
-    for (double x = kBeginingCycleInThirdTask; x <= kEndingCycleInThirdTask; x += kCycleStepInThirdTask) {
+    double x = 0;
+    while (x <= 1.) {
         int n = 0;
-        double y = pow(x, 2) * atan(x);
-        double s{};
-        /*double currentSummand{};*/
-        double currentSummand = kErrorRate;  // величина текущего слагаемого
+        double y = std::pow(x, 2) * std::atan(x);
 
-        while (currentSummand >= kErrorRate) {
-            currentSummand = pow(-1, n) * pow(x, 2 * n + 3) / (2 * n + 1);
+        double currentSummand = std::pow(x, 3);
+        double s = currentSummand;
+
+        while (std::abs(currentSummand) >= kEpsilon) {
+            currentSummand *= (-std::pow(x, 2) * (2 * n + 1) / (2 * n + 3));
             s += currentSummand;
             ++n;
         }
-        /*do {
-            currentSummand = pow(-1, n) * pow(x, 2 * n + 3) / (2 * n + 1);
-            s += currentSummand;
-            ++n;
-        } while (currentSummand >= kErrorRate);*/
 
-        std::cout << std::setw(kColumnWidth) << x << std::setw(kColumnWidth) << std::setprecision(kNumbersLength) << y << std::setw(kColumnWidth)
-                  << std::setprecision(kNumbersLength) << s << std::setw(kColumnWidth) << n << std::endl;
-    }
+        std::cout << std::setw(kColumnWidth) << x << std::setw(kColumnWidth) << std::setprecision(kPrecision) << y << std::setw(kColumnWidth)
+                  << std::setprecision(kPrecision) << s << std::setw(kColumnWidth) << n << std::endl;
 
-    if (SolveAgain()) {
-        DoThirdTask();
+        x += kXStep;
     }
 }
 
@@ -137,58 +107,64 @@ void DoFourthTask() {
     int n{};
     std::cout << "\nВведите натуральное число n." << std::endl;
     std::cin >> n;
-    n = n * 2 + 1;
 
-    double y = sqrt(n);
+    if (n <= 0) {
+        std::cout << "Неверный ввод. Число n должно быть натуральным." << std::endl;
+        return;
+    }
+
+    int k = n * 2 + 1;
+    double y = std::sqrt(k * 1.0);
     int summandsCounter = 1;
+    std::cout << y << std::endl;
 
-    while (n > 1) {
-        n -= 2;
-        ++summandsCounter;
-        y = sqrt(n + y);
-
-        if (summandsCounter == kThreeSummands) {
+    while (k > 1) {
+        /*if (n + 1 - summandsCounter == 3) {
             std::cout << "Промежуточный результат при 3 слагаемых:\ny = " << y << std::endl;
         } else if (summandsCounter == kFiveSummands) {
             std::cout << "Промежуточный результат при 5 слагаемых:\ny = " << y << std::endl;
         } else if (summandsCounter == kTenSummands) {
             std::cout << "Промежуточный результат при 10 слагаемых:\ny = " << y << std::endl;
-        }
+        }*/
+
+        k -= 2;
+        ++summandsCounter;
+        y = std::sqrt(k + y);
+        std::cout << y << std::endl;
     }
 
     std::cout << "Итоговый результат:\ny = " << y << std::endl;
-
-    if (SolveAgain()) {
-        DoFourthTask();
-    }
-}
-
-void TaskSelection() {
-    std::cout << "\nВведите номер задания (цифру от 1 до 4 включительно), которое хотите решить.\n"
-                 "Чтобы завершить программу, напишите 'e'."
-              << std::endl;
-
-    char taskNumber{};
-    std::cin >> taskNumber;
-
-    if (taskNumber == kFirstTaskSymbol) {
-        DoFirstTask();
-    } else if (taskNumber == kSecondTaskSymbol) {
-        DoSecondTask();
-    } else if (taskNumber == kThirdTaskSymbol) {
-        DoThirdTask();
-    } else if (taskNumber == kFourthTaskSymbol) {
-        DoFourthTask();
-    } else if (taskNumber == kExitProgramSymbol) {
-        return;
-    } else {
-        std::cout << "\nНеверный ввод, попробуйте заново.\n" << std::endl;
-        TaskSelection();
-    }
 }
 
 int main(int, char**) {
-    TaskSelection();
+    while (true) {
+        std::cout << "\nВведите номер задания (цифру от 1 до 4 включительно), которое хотите решить.\n"
+                     "Чтобы завершить программу, напишите 'e'."
+                  << std::endl;
+
+        char taskNumber{};
+        std::cin >> taskNumber;
+
+        switch (taskNumber) {
+            case kFirstTask:
+                DoFirstTask();
+                break;
+            case kSecondTask:
+                DoSecondTask();
+                break;
+            case kThirdTask:
+                DoThirdTask();
+                break;
+            case kFourthTask:
+                DoFourthTask();
+                break;
+            case kExitProgram:
+                return 0;
+            default:
+                std::cout << "\nНеверный ввод." << std::endl;
+                return 1;
+        }
+    }
 
     return 0;
 }
